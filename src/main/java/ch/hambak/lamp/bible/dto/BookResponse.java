f897bbb0f95cadabf763d69b258f7cf6df82140a
ned-razzz <smg9972@gmail.com>
2025-09-09 15:42:36 +0900

    fix: bean validation api dependency problem

--- src/main/java/ch/hambak/lamp/bible/dto/BookResponse.java
+++ src/main/java/ch/hambak/lamp/bible/dto/BookResponse.java
@@ -1,6 +1,8 @@
 package ch.hambak.lamp.bible.dto;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- src/main/java/ch/hambak/lamp/bible/dto/VerseResponse.java
+++ src/main/java/ch/hambak/lamp/bible/dto/VerseResponse.java
@@ -1,7 +1,7 @@
 package ch.hambak.lamp.bible.dto;
 
-//import jakarta.validation.constraints.NotBlank;
-//import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +13,9 @@ import lombok.NoArgsConstructor;
 @Builder
 public class VerseResponse {
 
-//    @NotNull
+    @NotNull
     private Short verse;
 
-//    @NotBlank
+    @NotBlank
     private String text;
 }
